923604f8415ca515999032b1180be5f5df7fc62c
ibrahim0891 <nijhum0891@gmail.com>
2025-10-10 20:12:57 +0600

    added backlink to home from 404 page

--- src/Pages/not-found.jsx
+++ src/Pages/not-found.jsx
@@ -3,13 +3,16 @@
 import React from 'react';
 import SectionHeading from './Global-components/SectionHeading';
 import Button from './Global-components/Button';
+import { Link } from 'react-router';
 
 const NotFound = () => {
     return (
         <div className='py-16 flex-1'>
             <img src="/assets/error-404.png" alt="" className='m-auto' />
-            <SectionHeading heading={'Oops, Page not found!'} subHeading={'The page you are looking for is not available.'}/>
-            <Button className={'m-auto block'}> Go Back! </Button>
+            <SectionHeading heading={'Oops, Page not found!'} subHeading={'The page you are looking for is not available.'} />
+            <Link to={'/'}>
+                <Button className={'m-auto block'}> Go Back! </Button>
+            </Link>
         </div>
     );
 };
